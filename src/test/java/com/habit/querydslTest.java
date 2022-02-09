629911ffe8baa0c2ed220af49e576398c87678f9
JiwanKim <jisungyeah@gmail.com>
2022-02-09 17:57:58 +0900

    test

--- src/test/java/com/habit/querydslTest.java
+++ src/test/java/com/habit/querydslTest.java
@@ -1,16 +1,8 @@
 package com.habit;
 
-import com.habit.domain.Login;
-import com.habit.repository.LoginRepository;
-import com.habit.repository.LoginRepositorySupport;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.security.acl.NotOwnerException;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,44 +10,70 @@ import java.util.regex.Pattern;
 public class querydslTest {
 
     @Test
-    public void querydsl_기본_테스트() {
-        String test = "ababcdcdababcdcd";
+    public void querydsl_기본_테스트() throws NotOwnerException {
+        //String test = "aabbaccc"; //8
+        //String test = "ababcdcdababcdcd"; //9
+        String test = "abcabcdede"; //8
+        //String test = "abcabcabcabcdededededede"; //14
+        //String test = "ababcdcdababcdcd"; //17
         int mediumNumber=test.length()/2;
         int maxLength = test.length();
         int leastNum=1;
         int startNum = 0;
-        String mixed = "";
-        for(int i=2;i<=mediumNumber;i++){
-            String beforeTemp=null;
+        List<String> stringArray = new ArrayList<>();
+        for(int i=2;i<mediumNumber;i++) {
+            String mixed = "";
+            String beforeTemp = null;
             int repeatNumber = 1;
             boolean repeatedBoolean = false;
-            for(int j=0;j<maxLength;j+=i){
-                if(beforeTemp==null) {
-                    beforeTemp = test.substring(j,i+j);
+            for (int j = 0; j < maxLength; j += i) {
+                if (j > maxLength) break;
+                if (beforeTemp == null) {
+                    beforeTemp = test.substring(j, i + j);
                     continue;
                 }
-                String temp = test.substring(j,i+j);
-                if(temp.equals(beforeTemp)){
-                    repeatedBoolean=true;
+                int sum = i + j;
+                if (sum > maxLength) sum = maxLength;
+                String temp = test.substring(j, sum);
+                if (temp.equals("")) break;
+                if (temp.equals(beforeTemp)) {
+                    repeatedBoolean = true;
                     beforeTemp = temp;
                     repeatNumber++;
-                }else{
-                    if(repeatedBoolean){
-                        mixed+=repeatNumber+beforeTemp;
-                        mixed+=temp;
-                    }else{
-                        mixed+=temp;
+                } else if(temp.equals("")){
+
+                } else {
+                    if (repeatedBoolean) {
+                        mixed += repeatNumber + beforeTemp;
+                    } else {
+                        if(mixed.equals("")){
+                            mixed += beforeTemp+temp;
+                        }else{
+                            mixed += temp;
+                        }
                     }
-                    repeatedBoolean=false;
-                    repeatNumber=1;
+                    repeatedBoolean = false;
+                    beforeTemp = temp;
+                    repeatNumber = 1;
                 }
-
-
-
-
             }
+            char[] chars = mixed.toCharArray();
+            boolean containsDigit = false;
+            for (char c : chars) {
+                if (Character.isDigit(c)) {
+                    containsDigit = true;
+                }
+            }
+            if (!containsDigit && mixed.length() != maxLength) continue;
+            stringArray.add(mixed);
         }
-        System.out.println(mixed);
+
+        System.out.println(stringArray);
+        int cnt = 0;
+        if(stringArray.size() == 0){ cnt = maxLength;
+        }else {
+            cnt = stringArray.stream().min(Comparator.comparing(d -> d.length())).orElseThrow(NotOwnerException::new).length();
+        }
         System.out.println("zcvcxvvcxv");
 
 
